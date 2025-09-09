0393af6bcb97c34f8594f20ea3783d579dbaf469
helpbirbal-cmyk <helpbirbal@gmail.com>
2025-09-10 00:26:28 +0530

    added vercel stuff  -aberdeen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@
 import { BlitzProvider } from "../blitz-client";
 import type { Metadata, Viewport } from "next";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
+
 
 export const metadata: Metadata = {
   title: "Your App",
